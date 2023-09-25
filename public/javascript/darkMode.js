84b842d900eeacb6d2f59ebe2feafa2ea4e8b1f1
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-09-25 11:38:01 +1000

    minor fixes

--- public/javascript/darkMode.js
+++ public/javascript/darkMode.js
@@ -1,13 +1,13 @@
 // Toggle dark mode
-let darkMode = false;
+// let darkMode = false;
 
-function toggleDarkMode() {
-  darkMode = !darkMode;
-  if (darkMode) {
-    document.body.classList.add("dark-mode");
-  } else {
-    document.body.classList.remove("dark-mode");
-  }
-}
-const darkModeToggleNav = document.querySelector("#dark-mode-toggle-nav");
-darkModeToggleNav.addEventListener("click", toggleDarkMode);
+// function toggleDarkMode() {
+//   darkMode = !darkMode;
+//   if (darkMode) {
+//     document.body.classList.add("dark-mode");
+//   } else {
+//     document.body.classList.remove("dark-mode");
+//   }
+// }
+// const darkModeToggleNav = document.querySelector("#dark-mode-toggle-nav");
+// darkModeToggleNav.addEventListener("click", toggleDarkMode);
